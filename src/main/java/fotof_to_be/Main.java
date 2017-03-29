8ba05a5344d02abb5247a99ca6dbcbce0e9a424e
dickow <dickow@jeppes-mbp.eduroam.wireless.dtu.dk>
2017-03-29 16:21:33 +0200

    Initial commit

---
+++ src/main/java/fotof_to_be/Main.java
@@ -0,0 +1,9 @@
+package fotof_to_be;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("Loan Approval App")
+public class Main extends ServletProcessApplication{
+
+}
